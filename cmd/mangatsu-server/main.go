80dbd89bac8d02badae8a607238dc9d446254317
Marko Leinikka <Luukuton@users.noreply.github.com>
2022-01-27 22:24:54 +0200

    feat: Add main.go

---
+++ cmd/mangatsu-server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/Mangatsu/server/internal/config"
+	"github.com/Mangatsu/server/pkg/api"
+	"github.com/Mangatsu/server/pkg/db"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	config.LoadEnv()
+	db.Initdb()
+	db.EnsureLatestVersion()
+
+	username, password := config.GetInitialAdmin()
+	users, _ := db.GetUser(username)
+	if users == nil || len(users) == 0 {
+		err := db.Register(username, password, int64(db.Admin))
+		if err != nil {
+			log.Fatal("Error registering initial admin: ", err)
+		}
+	}
+
+	// Parse libraries from the environmental and insert/update to the db.
+	libraries := config.ParseBasePaths()
+	db.StorePaths(libraries)
+
+	// Scan the libraries for metadata and insert/update to the db.
+	//startScan := time.Now()
+	//library.ScanArchives()
+	//metadata.ParseX()
+	//metadata.ParseTitles(true, false)
+	//elapsedScan := time.Since(startScan)
+	//log.Info("Finding and tagging archives took: ", elapsedScan)
+
+	// Generate thumbnails for all the archives, prioritizing covers.
+	//startThumbnail := time.Now()
+	//library.GenerateThumbnails(true)
+	//elapsedThumbnail := time.Since(startThumbnail)
+	//log.Info("Generating thumbnails took: ", elapsedThumbnail)
+
+	api.LaunchAPI()
+}
