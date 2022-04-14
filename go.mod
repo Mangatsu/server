06b38d8870a77d5b4f8de5217e261b8182fb22d2
Marko Leinikka <Luukuton@users.noreply.github.com>
2022-04-15 01:10:34 +0300

    feat: Add abstract cache for galleries
    Supports mutex and pruning old entries based on access times.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/adrg/strutil v0.2.3
 	github.com/chai2010/webp v1.1.1
 	github.com/disintegration/imaging v1.6.2
+	github.com/djherbis/atime v1.1.0
 	github.com/facette/natsort v0.0.0-20181210072756-2cd4dd1e2dcb
 	github.com/go-jet/jet/v2 v2.7.1
 	github.com/golang-jwt/jwt/v4 v4.4.1
@@ -17,7 +18,7 @@ require (
 	github.com/pressly/goose/v3 v3.5.3
 	github.com/rs/cors v1.8.2
 	github.com/sirupsen/logrus v1.8.1
-	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 )
 
 require (
@@ -35,8 +36,9 @@ require (
 	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/therootcompany/xz v1.0.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
-	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
-	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64 // indirect
+	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ pkg/cache/cache.go
@@ -0,0 +1,138 @@
+package cache
+
+import (
+	"github.com/Mangatsu/server/internal/config"
+	"github.com/Mangatsu/server/pkg/library"
+	"github.com/djherbis/atime"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"path"
+	"sync"
+	"time"
+)
+
+type cacheValue struct {
+	Accessed time.Time
+	Mu       *sync.Mutex
+}
+
+type GalleryCache struct {
+	Path  string
+	Store map[string]cacheValue
+}
+
+var galleryCache *GalleryCache
+
+// Init initializes the abstraction layer for the gallery cache.
+func Init() {
+	galleryCache = &GalleryCache{
+		Path:  config.BuildCachePath(),
+		Store: make(map[string]cacheValue),
+	}
+
+	iterateCacheEntries(func(pathToEntry string, accessTime time.Time) {
+		maybeUUID := path.Base(pathToEntry)
+		if _, err := uuid.Parse(maybeUUID); err != nil {
+			return
+		}
+
+		galleryCache.Store[path.Base(pathToEntry)] = cacheValue{
+			Accessed: accessTime,
+			Mu:       &sync.Mutex{},
+		}
+	})
+}
+
+// PruneCache removes entries not accessed (internal timestamp in mem) in the last ttlSeconds in a thread-safe manner.
+func PruneCache(ttlSeconds int32) {
+	now := time.Now()
+	for galleryUUID, value := range galleryCache.Store {
+		value.Mu.Lock()
+		if value.Accessed.Add(time.Second * time.Duration(ttlSeconds)).Before(now) {
+			if err := remove(galleryUUID); err != nil {
+				log.Errorf("Error occured while deleting cache entry: %s", err)
+			}
+		}
+		value.Mu.Unlock()
+	}
+}
+
+// PruneCacheFS removes entries not accessed (filesystem timestamp) in the last ttlSeconds. Not thread-safe.
+func PruneCacheFS(ttlSeconds int32) {
+	now := time.Now()
+	iterateCacheEntries(func(pathToEntry string, accessTime time.Time) {
+		if accessTime.Add(time.Second * time.Duration(ttlSeconds)).Before(now) {
+			if err := os.Remove(pathToEntry); err != nil {
+				log.Errorf("Error occured while removing cache entry: %s", err)
+			}
+		}
+	})
+}
+
+// Read reads the cached gallery from disk. If it doesn't exist, it will be created and then read.
+func Read(archivePath string, galleryUUID string) ([]string, int) {
+	galleryCache.Store[galleryUUID] = cacheValue{
+		Accessed: time.Now(),
+		Mu:       &sync.Mutex{},
+	}
+
+	galleryCache.Store[galleryUUID].Mu.Lock()
+	defer galleryCache.Store[galleryUUID].Mu.Unlock()
+
+	files, count := library.ReadGallery(archivePath, galleryUUID)
+	if count == 0 {
+		return files, count
+	}
+
+	return files, count
+}
+
+// remove wipes the cached gallery from disk.
+func remove(galleryUUID string) error {
+	// Paranoid check to make sure that the base is a real UUID, since we don't want to delete anything else.
+	maybeUUID := path.Base(galleryUUID)
+	if _, err := uuid.Parse(maybeUUID); err != nil {
+		delete(galleryCache.Store, galleryUUID)
+		return err
+	}
+
+	galleryPath := config.BuildCachePath(galleryUUID)
+	if err := os.RemoveAll(galleryPath); err != nil {
+		if os.IsNotExist(err) {
+			delete(galleryCache.Store, galleryUUID)
+		}
+		return err
+	}
+
+	delete(galleryCache.Store, galleryUUID)
+
+	return nil
+}
+
+// iterateCacheEntries iterates over all cache entries and calls the callback function for each entry.
+func iterateCacheEntries(callback func(pathToEntry string, accessTime time.Time)) {
+	cachePath := config.BuildCachePath()
+	cacheEntries, err := os.ReadDir(cachePath)
+	if err != nil {
+		log.Errorf("Could not read cache dir: %s", err)
+		return
+	}
+
+	for _, entry := range cacheEntries {
+		info, err := entry.Info()
+		if err != nil {
+			log.Errorf("Error occured while reading cache entry info: %s", err)
+			return
+		}
+
+		pathToEntry := path.Join(cachePath, entry.Name())
+		accessTime, err := atime.Stat(pathToEntry)
+		if err != nil {
+			log.Debugf("Could not read the access time of '%s'.  %s", entry.Name(), err)
+			accessTime = info.ModTime()
+		}
+
+		callback(pathToEntry, accessTime)
+	}
+}
