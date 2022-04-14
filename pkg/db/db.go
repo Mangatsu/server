8aef56ea7444d9157689b465003d5390bf0d14e5
Marko Leinikka <Luukuton@users.noreply.github.com>
2022-04-15 00:27:25 +0300

    feat: Add support for binary embedded migrations

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"embed"
 	"github.com/Mangatsu/server/internal/config"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/pressly/goose/v3"
@@ -9,6 +10,9 @@ import (
 )
 
 var database *sql.DB
+
+//go:embed migrations
+var embedMigrations embed.FS
 
 // Initdb initializes the database
 func Initdb() {
@@ -25,12 +29,15 @@ func db() *sql.DB {
 
 // EnsureLatestVersion ensures that the database is at the latest version by running all migrations.
 func EnsureLatestVersion() {
+	// For embedding the migrations in the binary.
+	goose.SetBaseFS(embedMigrations)
+
 	err := goose.SetDialect("sqlite3")
 	if err != nil {
 		log.Fatal("Invalid DB driver", "driver", "sqlite3", err)
 	}
 
-	err = goose.Run("up", db(), "./pkg/db/migrations")
+	err = goose.Up(db(), "migrations")
 	if err != nil {
 		log.Fatal("Failed to apply new migrations", err)
 	}
