19d94775e72b12ebd764f70a680191f7421703d9
Marko Leinikka <mleinikka@gmail.com>
2024-01-09 02:27:34 +0900

    feat: add tif and heif image exts

--- pkg/constants/regex.go
+++ pkg/constants/regex.go
@@ -4,4 +4,4 @@ import "regexp"
 
 var ArchiveExtensions = regexp.MustCompile(`\.(?:zip|cbz|rar|cbr|7z)$`)
 var MetaExtensions = regexp.MustCompile(`\.(?:json|txt)$`)
-var ImageExtensions = regexp.MustCompile(`\.(?:jpe?g|png|webp|bmp|gif|tiff)$`)
+var ImageExtensions = regexp.MustCompile(`\.(?:jpe?g|png|webp|bmp|gif|tiff?|heif)$`)
