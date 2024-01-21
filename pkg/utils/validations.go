19d04ebc776083f144fae013583c94f1f2d90e47
Marko Leinikka <mleinikka@gmail.com>
2024-01-21 22:03:58 +0900

    feat: allow numbers as the first character in usernames

--- pkg/utils/validations.go
+++ pkg/utils/validations.go
@@ -7,8 +7,8 @@ import (
 // Alphanumeric characters of any language, dashes, underscores, spaces, and special characters in the session name.
 var wideRe = regexp.MustCompile(`^[\p{L}\p{N}\p{Pd}\p{Pc}\p{Zs}\p{Sc}\p{Sk}!?@#$%^&*+]+$`)
 
-// The username can contain alphanumeric characters (first character must be a letter), dashes and underscores.
-var usernameRe = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_-]{1,31}$`)
+// The username can contain alphanumeric characters, dashes and underscores.
+var usernameRe = regexp.MustCompile(`^[a-zA-Z0-9_-]$`)
 
 // The password can contain almost all characters except control characters, whitespace and quotes.
 var passwordRe = regexp.MustCompile(`^[^\x00-\x1F\x7F\s'"]+$`)
