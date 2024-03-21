a205d4c40131f202e712599307fac8aa3bd41538
Marko Leinikka <mleinikka@gmail.com>
2024-03-21 13:55:47 +0900

    feat: add avif to supported gallery image formats

--- pkg/constants/regex.go
+++ pkg/constants/regex.go
@@ -4,4 +4,4 @@ import "regexp"
 
 var ArchiveExtensions = regexp.MustCompile(`\.(?:zip|cbz|rar|cbr|7z)$`)
 var MetaExtensions = regexp.MustCompile(`\.(?:json|txt)$`)
-var ImageExtensions = regexp.MustCompile(`\.(?:jpe?g|png|webp|bmp|gif|tiff?|heif)$`)
+var ImageExtensions = regexp.MustCompile(`\.(?:jpe?g|png|webp|avif|bmp|gif|tiff?|heif)$`)
